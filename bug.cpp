4b3bf30e0abec65c12c121138b91a7b1f8ca9c26
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2021-12-22 17:59:52 +0530

    Add files via upload

---
+++ bug.cpp
@@ -0,0 +1,14 @@
+// Now you should not forgert your glasses
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "if this text" << endl;
+    cout << "apperas on your display\n";
+    cout << "you can pat yourself on";
+    cout << "the back!" << endl;
+
+    return 0;
+}
+
