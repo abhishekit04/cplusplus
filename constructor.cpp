9c0c6bf529584325ae087c1510d04ade84b65f4b
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2021-12-27 18:16:59 +0530

    Update constructor.cpp

--- constructor.cpp
+++ constructor.cpp
@@ -1,44 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+class Shape
+{
 
-class Shape {
-	
-	private:
-		
+private:
     double radius;
     double length;
     double width;
-public:
-    Shape(double r,double l, double w){
-       radius=r; 
-	   length=l;
-	   width=w;
-}
-  public:
-	double AreaCircle()
-	{
-		return 3.14*radius*radius;
-	}
 
-public:	
-	double AreaRectangle()
-	{
-		return length*width;
-	}
-	
+public:
+    Shape(double r, double l, double w)
+    {
+        radius = r;
+        length = l;
+        width = w;
+    }
+
+public:
+    double AreaCircle()
+    {
+        return 3.14 * radius * radius;
+    }
+
+public:
+    double AreaRectangle()
+    {
+        return length * width;
+    }
 };
 
 int main()
 {
-	Shape s1(10,10,20);
-	cout<<("Area of circle: ");
-	cout<<(s1.AreaCircle());
-	cout<<endl;
-	cout<<("Area of rectangle: ");
-	cout<<(s1.AreaRectangle());
-	return 0;
+    Shape s1(10, 10, 20);
+    cout << ("Area of circle: ");
+    cout << (s1.AreaCircle());
+    cout << endl;
+    cout << ("Area of rectangle: ");
+    cout << (s1.AreaRectangle());
+    return 0;
 }
-
-
-
