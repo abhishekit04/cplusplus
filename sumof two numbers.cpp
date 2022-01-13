61364665b16625a032d21e9943c8d0941bbf7fb4
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2022-01-13 19:04:05 +0530

    Add files via upload

---
+++ sumof two numbers.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+	int num1, num2, sum;
+	printf("enter first number and secound number");
+	scanf("%d %d",&num1,&num2);
+	sum= num1 + num2 ;
+ 	
+	
+	
+	printf("Num1 = %d\n",num1);
+	printf("num2 = %d\n",num2);
+	printf("Sum = %d\n", sum);
+	
+}
