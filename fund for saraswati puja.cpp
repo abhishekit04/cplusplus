da854a45b1270bc90a1062c5354ca0085d396890
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2022-01-14 11:49:14 +0530

    Add files via upload

---
+++ fund for saraswati puja.cpp
@@ -0,0 +1,22 @@
+//fund for saraswati puja
+
+#include<stdio.h>
+int main () {
+	int  minal , kittu , elina , surbhi , pooja , total;
+	float kunal , lovely , praansho ;
+	praansho = 874.4 ;
+	kunal = 987.14 ;
+	minal = 555 ;
+	lovely = 452.4 ;
+	kittu = 984 ;
+	elina = 547 ; 
+	surbhi = 748 ;
+	pooja = 659 ;
+	total = minal + kittu + elina + surbhi + pooja + kunal + lovely + praansho;
+	//sum of this funds */
+	
+	printf("sum of %d and %d and %d and %d and %d and %f and %f and %f = %d " , minal , kittu , elina , surbhi , pooja , kunal , lovely , praansho , total);
+	
+	
+	 
+}
