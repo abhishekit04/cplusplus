b569256e6086b1cc8ba183eacfacf5a8d7ca21df
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-11 00:53:17 +0530

    Add files via upload

---
+++ positive or negative number.cpp
@@ -0,0 +1,25 @@
+// Program to find whether a number is positive or negative
+
+#include<stdio.h>
+#include<conio.h>
+
+int main () {
+	int number;
+	printf("Enter a number :\n");
+	scanf("%d", &number);
+	
+	if(number > 0)
+	{
+		printf("Number is Positive");
+	}
+	else
+	{
+		printf("Number is Negative");
+	}
+	
+	
+	return 0;
+	
+}
+	
+	
