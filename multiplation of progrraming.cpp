42d66f07b900e8184cd9be52f2052f2b9ccf5800
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-11 00:49:42 +0530

    Add files via upload

---
+++ multiplation of progrraming.cpp
@@ -0,0 +1,14 @@
+// program of multiplication of two numbers 
+#include<stdio.h>
+
+int main() {
+	int num1 , num2 , product;
+	
+	num1 = 540;
+	num2 = 400;
+	product = num1 * num2 ;
+	/* this statement outputs the result */
+	 
+	
+	printf("product of %d and %d = %d", num1,num2,product);
+}
