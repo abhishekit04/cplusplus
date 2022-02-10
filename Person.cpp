3bc874c143a648cb4c5a0d69282905a17adf0800
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-11 00:52:04 +0530

    Add files via upload

--- Person.cpp
+++ Person.cpp
@@ -2,45 +2,43 @@
 using namespace std;
 
 
-class Person {
-    char name[30];
-    int roll;
-    int fee;
+class Shape {
+	
+	private:
+		
+    double radius;
+    double length;
+    double width;
+public:
+    Shape(double r,double l, double w){
+       radius=r; 
+	   length=l;
+	   width=w;
+}
+  public:
+	double AreaCircle()
+	{
+		return 3.14*radius*radius;
+	}
 
-    public:
-        void setData(void);
-        void getData(void);
+public:	
+	double AreaRectangle()
+	{
+		return length*width;
+	}
+	
 };
 
-void Person:: setData(void) {
-    cout << "Enter Name : ";
-    cin >> name;
-    cout << "Enter roll : ";
-    cin >> roll;
-    cout << "Enter fee : ";
-    cin >> fee;
+int main()
+{
+	Shape s1(10,10,20);
+	cout<<("Area of circle: ");
+	cout<<(s1.AreaCircle());
+	cout<<endl;
+	cout<<("Area of rectangle: ");
+	cout<<(s1.AreaRectangle());
+	return 0;
 }
 
-void Person:: getData(void) {
-    cout << "\nName = " << name;
-    cout << "\nroll = " << roll;
-     cout << "\nfee = " << fee;
-}
 
-int main() {
 
-    Person p1,p2,p3,p4,p5;   //object
-    
- 	p1.setData();
- 	p2.setData();
- 	p3.setData();
- 	p4.setData();
- 	p5.setData();
-    p1.getData();
-    p2.getData();
-    p3.getData();
-    p4.getData();
-    p5.getData();
-
-    return 0;
-}
