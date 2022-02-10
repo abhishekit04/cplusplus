c3d4851e77b2c6d98064fbbc6eff26f4718dee1c
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-11 00:35:25 +0530

    Add files via upload

---
+++ addition of two numbes.cpp
@@ -0,0 +1,15 @@
+// C program for addition of two numbers
+
+#include<stdio.h>
+#include<conio.h>
+
+int main () {
+	int  num1, num2, sum;
+    
+	num1 = 50;
+	num2 = 40;
+	sum = num1 + num2 ;
+	//sum of two numbers //
+	
+	printf("%d and %d = %d", num1 , num2, sum);    	 
+}
