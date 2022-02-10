2494a9695f925a0e6018d263fecf6936f7e1154e
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-11 00:42:32 +0530

    Add files via upload

---
+++ division of this program.cpp
@@ -0,0 +1,17 @@
+// Division of programming 
+
+#include<stdio.h>
+
+int main () {
+	float num1, num2, div;
+	num1 = 50;
+	num2 = 3;
+	div = num1 / num2;
+	/* division of this product */
+	
+	printf("div of %f and %f = %f" , num1, num2, div);
+	scanf("%f%f",&num1, &num2);
+	div = num1 / num2;
+	printf("div of %f and %f = %f" , num1, num2, div);
+	
+}
