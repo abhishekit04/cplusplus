93fa48180c5f26f54b55be29b57610233afcc9e3
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-11 00:39:10 +0530

    Add files via upload

---
+++ average of programming.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main () {
+	int num1 , num2 , num3 , sum; 
+	float avg;
+	scanf(" %d%d%d", &num1 ,&num2, &num3);
+	
+	sum = num1 + num2 + num3;
+	avg = sum / (float)3;
+	
+	printf("Average of %d , %d and %d is %.2f", num1 , num2, num3, avg);
+	
+	return 0;
+}
