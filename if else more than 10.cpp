7ceca3c4be2c7c6844108697e261074b004e04e8
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-11 00:45:58 +0530

    Add files via upload

---
+++ if else more than 10.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main () {
+	int number;
+	printf("Enter a number : \n");
+	scanf("%d",&number);
+	if(number> 10)
+	{
+		printf("number is greater than 10");
+		
+	}
+	else
+	{
+		printf("number is not greater than 10 or number is less than 10");
+		
+	}
+	return 0;
+}
