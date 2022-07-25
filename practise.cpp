955f7740098850c4e8123c61b0f0fd6fa4aae245
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2022-07-25 23:18:45 +0530

    Add files via upload

---
+++ practise.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char str[40];
+	int m1, m2, m3,avg;
+	
+	cout<<"Enter your name";
+	cin>> str;
+	cout<<"Enter the marks of three subjects:";
+	cin>>m1>>m2>>m3;
+	avg=(m1+m2+m3)/3;
+	
+	cout << "Your name is " <<str;
+	cout<<endl<<"And your average marks are "<<avg<<endl;
+	return 0;
+}
