0ccca7b814a1282d8eb1e1c269b0e65ac23ed001
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2022-07-29 00:48:10 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int n , cube;
+
+    cout << "Enter the number:";
+    cin >> n;
+
+    cube = n*n*n;
+
+    cout << "cube of:" << n << "is:" << cube << endl; 
+    
+    return 0;
+}
