c0919150802abdc3b93b99a3932f26fe2748110d
Abhishek Singh <88192065+abhishekit04@users.noreply.github.com>
2023-09-21 22:19:33 +0530

    Add files via upload

--- overloading.cpp
+++ overloading.cpp
@@ -1,29 +1,21 @@
-// overloading class constructors
 #include <iostream>
 using namespace std;
- 
-class Rectangle {
-    int width, height;
-  public:
-    Rectangle ();
-    Rectangle (int,int);
-    int area (void) {return (width*height);}
+class Cal
+{
+public:
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+    static int add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
 };
- 
-Rectangle::Rectangle () {
-  width = 5;
-  height = 5;
-}
- 
-Rectangle::Rectangle (int a, int b) {
-  width = a;
-  height = b;
-}
- 
-int main () {
-  Rectangle rect (3,4);
-  Rectangle rectb;
-  cout << "rect area: " << rect.area() << endl;
-  cout << "rectb area: " << rectb.area() << endl;
-  return 0;
+int main(void)
+{
+    Cal C; //     class object declaration.
+    cout << C.add(10, 20) << endl;
+    cout << C.add(12, 20, 23);
+    return 0;
 }
